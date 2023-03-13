2f87a5bf120e1eb09c95909911caf06abd1c3c33
mingtiao <xiaoxingnianliu@gmail.com>
2023-03-13 17:01:21 +0800

    重新提交

---
+++ unpackage/dist/dev/mp-weixin/subpkg/goods_detail/goods_detail.js
@@ -0,0 +1,31 @@
+(global["webpackJsonp"] = global["webpackJsonp"] || []).push([["subpkg/goods_detail/goods_detail"],{
+
+/***/ 41:
+/*!**********************************************************************************************!*\
+  !*** D:/xiaochengxu/day1/uni-shop-2/main.js?{"page":"subpkg%2Fgoods_detail%2Fgoods_detail"} ***!
+  \**********************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+/* WEBPACK VAR INJECTION */(function(createPage) {__webpack_require__(/*! uni-pages */ 4);
+var _vue = _interopRequireDefault(__webpack_require__(/*! vue */ 2));
+var _goods_detail = _interopRequireDefault(__webpack_require__(/*! ./subpkg/goods_detail/goods_detail.vue */ 42));function _interopRequireDefault(obj) {return obj && obj.__esModule ? obj : { default: obj };}
+createPage(_goods_detail.default);
+/* WEBPACK VAR INJECTION */}.call(this, __webpack_require__(/*! ./node_modules/@dcloudio/uni-mp-weixin/dist/index.js */ 1)["createPage"]))
+
+/***/ }),
+
+/***/ 42:
+/*!***************************************************************************!*\
+  !*** D:/xiaochengxu/day1/uni-shop-2/subpkg/goods_detail/goods_detail.vue ***!
+  \***************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+throw new Error("Module build failed (from ./node_modules/@dcloudio/webpack-uni-mp-loader/lib/style.js):\nError: ENOENT: no such file or directory, open 'D:\\xiaochengxu\\day1\\uni-shop-2\\subpkg\\goods_detail\\goods_detail.vue'");
+
+/***/ })
+
+},[[41,"common/runtime","common/vendor"]]]);
+//# sourceMappingURL=../../../.sourcemap/mp-weixin/subpkg/goods_detail/goods_detail.js.map
